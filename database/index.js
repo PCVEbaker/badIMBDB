c0dceff71fc3152b7a74a04b34a1bce5f7bfcee6
Evan Baker <3van90@gmail.com>
2017-12-22 13:20:54 -0500

    Set up database and create function to add movies to database

---
+++ database/index.js
@@ -0,0 +1,38 @@
+const data = require('../dummyData');
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/movies');
+
+const movieSchema = mongoose.Schema({
+  moviename: String,
+  voteAvg: Number,
+  imgPath: String,
+  id: Number
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+
+// Function will iterate over array of movies, check if the movie already exists
+// in the database, and if it doesn't add it to the database
+const save = function(movie, cb) {
+  Movie.find({"id": movie.id}, (err, docs) => {
+    if (err) {
+      console.log("Error: ", err);
+    }
+    if (docs.length === 0) {
+      let instance = new Movie({
+        "moviename": movie.title,
+        "voteAvg": movie.vote_average,
+        "imgPath": movie.poster_path,
+        "id": movie.id
+      });
+      instance.save((err, products, numAffected) => {
+
+      });
+    }
+  })
+};
+
+
+module.exports.save = save;
