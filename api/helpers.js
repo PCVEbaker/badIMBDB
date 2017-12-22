11a327e3b42593e139f0edac7afad5fb7e3ddcfc
Evan Baker <3van90@gmail.com>
2017-12-22 13:44:21 -0500

    Handle get requests to /movies properly

--- api/helpers.js
+++ api/helpers.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const getPopularMovies = function(cb) {
+const getMovies = function(cb) {
   axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key=654f5d8f68846599da7d80021ad2ba27')
     .then((response) => {
       cb(response.data.results);
@@ -10,4 +10,4 @@ const getPopularMovies = function(cb) {
     })
 };
 
-module.exports.getPopularMovies = getPopularMovies
+module.exports.getMovies = getMovies
--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const save = require('../database/index.js');
-const getMovies = require('../api/helpers.js');
+const getMovies = require('../api/helpers.js').getMovies;
 
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
+app.get('/movies', (req, res) => {
+  getMovies((data) => {
+    res.json(data);
+  })
+});
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
